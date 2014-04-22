2263bfa001f287c0aa62026d40326b2fce2babc7
Ross Nicoll <jrn@jrn.me.uk>
2014-04-23 00:18:06 +0100

    Reworked servlet structure to use own Freemarker support servlet, as default appears incompatible with Tomcat 8.

--- src/main/java/org/lostics/payment_protocol/servlet/AbstractServlet.java
+++ src/main/java/org/lostics/payment_protocol/servlet/AbstractServlet.java
@@ -1,25 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.lostics.payment_protocol.servlet;
 
-import freemarker.ext.servlet.FreemarkerServlet;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
 import java.util.Map;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import freemarker.ext.beans.BeansWrapper;
+import freemarker.template.Configuration;
+import freemarker.template.SimpleScalar;
+import freemarker.template.Template;
+import freemarker.template.TemplateBooleanModel;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
 
 /**
  *
  * @author jrn
  */
-public abstract class AbstractServlet extends FreemarkerServlet {
+public abstract class AbstractServlet extends HttpServlet {
+    public static final Charset CHARSET_UTF_8 = Charset.forName("UTF-8");
+    public static final String CONTEXT_PARAMETER_FREEMARKER_TEMPLATE_DIR = "org.lostics.payment_protocol.servlet.freemarkerTemplateDir";
+    
+    public static final String DEFAULT_FREEMARKER_TEMPLATE_DIR = "/WEB-INF/freemarker";
+    
+    private Configuration freemarkerCfg;
+    
+    public final Map<String, Object> buildRoot(final HttpServletRequest request,
+        final HttpServletResponse response)
+        throws ServletException {
+        final Map<String, Object> root = new HashMap<>();
+        
+        root.put("request", request);
+        root.put("response", response);
+        
+        return root;
+    }
+
+    private freemarker.template.Configuration configureFreemarker(final ServletContext context, final String templateDirectory)
+        throws ServletException {
+        final freemarker.template.Configuration freemarkerConfiguration = new freemarker.template.Configuration();
+
+        try {
+            final File abstractTemplateDir = new File(context.getRealPath(templateDirectory));
+
+            if (false == abstractTemplateDir.exists() ||
+                false == abstractTemplateDir.isDirectory()) {
+                throw new ServletException("Supplied Freemarker template directory \""
+                    + abstractTemplateDir.getCanonicalPath() + "\" (generated from \""
+                    + templateDirectory + "\") is not a directory.");
+            }
+            freemarkerConfiguration.setDirectoryForTemplateLoading(abstractTemplateDir);
+            freemarkerConfiguration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+            freemarkerConfiguration.setOutputEncoding(CHARSET_UTF_8.name());
+        } catch(IOException e) {
+            throw new ServletException(e);
+        }
+
+        final BeansWrapper wrapper = new BeansWrapper();
+        wrapper.setExposureLevel(BeansWrapper.EXPOSE_SAFE);
+        freemarkerConfiguration.setObjectWrapper(wrapper);
+
+        return freemarkerConfiguration;
+    }
+
+    @Override
+    public void init(final ServletConfig config)
+        throws ServletException {
+        super.init(config);
+
+        final ServletContext context = config.getServletContext();
+        String templateDirectory = context.getInitParameter(CONTEXT_PARAMETER_FREEMARKER_TEMPLATE_DIR);
+
+        if (null == templateDirectory) {
+            templateDirectory = DEFAULT_FREEMARKER_TEMPLATE_DIR;
+        }
+        
+        this.freemarkerCfg = configureFreemarker(context, templateDirectory);
+    }
+    
     @Override
     public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException
     {
@@ -79,7 +144,7 @@ public abstract class AbstractServlet extends FreemarkerServlet {
             final Map<String, Object> root)
             throws Exception;
 
-    private Map<String, Object> buildRoot(HttpServletRequest request, HttpServletResponse response) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Configuration getConfiguration() {
+        return this.freemarkerCfg;
     }
 }
