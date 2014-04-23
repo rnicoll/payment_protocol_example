9e1f47ba8b7b017fe1f7cc3d24c58691dc932063
Ross Nicoll <jrn@jrn.me.uk>
2014-04-23 23:41:54 +0100

    Started adding form fields.

--- src/main/java/org/lostics/payment_protocol/servlet/DefaultServlet.java
+++ src/main/java/org/lostics/payment_protocol/servlet/DefaultServlet.java
@@ -10,6 +10,12 @@ import javax.servlet.http.HttpServletResponse;
  * @author jrn
  */
 public class DefaultServlet extends AbstractServlet {
+    public static final String NETWORK_BITCOIN_MAIN = "main";
+    public static final String NETWORK_BITCOIN_TEST = "test";
+    public static final String NETWORK_DOGECOIN_MAIN = "doge-main";
+    public static final String NETWORK_DOGECOIN_TEST = "doge-test";
+    
+    public static final long EXPIRE_INTERVAL = 60 * 60 * 1000; // One hour
 
     @Override
     public Template doGet(HttpServletRequest request, HttpServletResponse response, Map<String, Object> root) throws Exception {
--- src/main/webapp/WEB-INF/freemarker/default.ftl
+++ src/main/webapp/WEB-INF/freemarker/default.ftl
@@ -1,6 +1,21 @@
 <#assign title = "Test"/>
 <#escape x as x?xml>
 <#include "head.ftl" />
-<p>Test</p>
+
+<form action="" method="post">
+
+<p>Network:
+<select name="network">
+<option value="main">Main</option>
+<option value="test">Test</option>
+</select></p>
+
+
+<p>Memo: <input type="text" name="memo" /></p>
+
+<p><input type="submit" /></p>
+
+</form>
+
 <#include "foot.ftl" />
 </#escape>
