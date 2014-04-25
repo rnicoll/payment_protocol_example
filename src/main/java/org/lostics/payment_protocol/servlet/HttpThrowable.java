1669824599690a40c32468bb0f786d13576605e8
Ross Nicoll <jrn@jrn.me.uk>
2014-04-25 21:37:44 +0100

    Added structures for aborting servlet execution with a non-200 status code return, using a throwable object.

---
+++ src/main/java/org/lostics/payment_protocol/servlet/HttpThrowable.java
@@ -0,0 +1,51 @@
+package org.lostics.payment_protocol.servlet;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Throwable for returning HTTP statuses outwith the normal flow of execution
+ * for code. Used as a way of ensuring execution of a servlet stops immediately,
+ * rather than risking typos causing status code to be set and execution to then
+ * continue.
+ */
+public class HttpThrowable extends Throwable {
+    private final int statusCode;
+    
+    public          HttpThrowable(final int statusCode) {
+        super();
+        
+        if (statusCode < HttpServletResponse.SC_CONTINUE
+                || statusCode >= 600) {
+            throw new IllegalArgumentException("HTTP status codes must be in the range 100-599 inclusive.");
+        }
+        
+        this.statusCode = statusCode;
+    }
+    
+    public          HttpThrowable(final int statusCode, final String message) {
+        super(message);
+        
+        if (statusCode < HttpServletResponse.SC_CONTINUE
+                || statusCode >= 600) {
+            throw new IllegalArgumentException("HTTP status codes must be in the range 100-599 inclusive.");
+        }
+        
+        this.statusCode = statusCode;
+    }
+    
+    public void send(final HttpServletResponse response)
+        throws IllegalStateException, IOException {
+        response.reset();
+    
+        if (null != this.getMessage()) {
+            response.sendError(getStatusCode(), this.getMessage());
+        } else {
+            response.setStatus(this.getStatusCode());
+        }
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
